44eec51cae8606671bcdd9acc17661e31427343b
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-05-17 22:03:00 -0400

    Changed home page styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Route, Link, Switch} from "react-router-dom";
 import './App.css';
 import About from "./components/About";
@@ -6,17 +6,32 @@ import Projects from "./components/Projects";
 import Home from "./components/Home";
 
 function App() {
-  return (
+    const [h1Position, setH1position] = useState({ 
+        position: "initial"
+});
+    const moveH1 = () => {
+        if (h1Position.position === "initial") {
+            return setH1position({position: "right"})
+        } else if (h1Position.position === "left") {
+            return setH1position({position: "right"})
+        } else {
+            return setH1position({position: "left"})
+        }
+    }
+    return (
         <div className="App">
             <div className="App-body">
-                <header className="header">
-                    
+                <header className="header"> 
                     <div className="header-nav">
                         <Link to="/">Home</Link>
                         <Link to= "/projects">Projects</Link>
                         <Link to="/about">About Me</Link>
                     </div>
-                    <h1>Rodolfo Jaspe</h1>
+                    <div className="h1-div">
+                        <div onMouseEnter={moveH1} className={h1Position.position}>
+                            <h1>Rodolfo Jaspe</h1>
+                        </div>
+                    </div>
                 </header>
                 <main>
                     <Switch>
@@ -27,7 +42,7 @@ function App() {
                 </main>
             </div>
         </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,15 @@ import bridge from "../Assets/bridge.jpg";
 import "./Home.css"
 
 function Home () {
+
     return (
         <div className="home">
-            <div>
-                <img src= {bridge} alt="bridge" />
-            </div>
-            <div>
-                <div className="home-projects">
+            <div className="img-and-app-row">
+                <div className="img-div">
+                    <img src= {bridge} alt="bridge" />
+                </div>
+                <div>
+                    <div className="home-projects">
                     <h3>
                         <a href="https://hilites.netlify.app/">HiLites App</a>
                     </h3>
@@ -19,9 +21,9 @@ function Home () {
                     <h3><a href="https://toodue.netlify.app/">TooDue App</a></h3>
                     <iframe src ="https://toodue.netlify.app/" title="latest soccer highlights"></iframe>         
                 </div>
-            </div>
-            
-        
+                </div>
+                
+            </div>      
         </div>
     )
 }
